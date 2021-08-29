4b797cc690178246877357334f306394788720b4
Shivamcool400 <66465485+Shivamcool400@users.noreply.github.com>
2021-08-30 01:19:09 +0530

    1

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,26 @@
 const result = document.querySelector('.result')
+
+const fetchData = async ()=> {
+    try {
+ const {data } = await axios.get('/api/2-basic-api');
+ const products = data.map((products)=> {
+     const {image:{url},name,price} = products
+     return `<article class="product">
+     <img
+       src="${url}"
+       alt="${name}"
+     />
+     <div class="info">
+       <h5>${name}</h5>
+       <h5 class="price">$${price}</h5>
+     </div>
+   </article>`
+ }).join('')
+      result.innerHTML = products
+    } catch (error) {
+        result.innerHTML = `<h4> there was an error</h4>`
+    }
+}
+
+
+fetchData()
---
+++ functions/1-hello.js
@@ -0,0 +1,8 @@
+
+
+exports.handler = async (event,context, cb) => {
+    return {
+        statusCode:200,
+        body:'our first fnc example',
+    }
+}
