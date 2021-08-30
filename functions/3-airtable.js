13e01eabdd1954a37afe7b409d791ba5ccb535a3
Shivamcool400 <66465485+Shivamcool400@users.noreply.github.com>
2021-08-30 20:56:58 +0530

    commit

---
+++ functions/3-airtable.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const Airtable = require('airtable-node')
+
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY})
+.base('appqm1XggMgz4p1jP')
+.table('products')
+
+
+
+exports.handler = async (event,context, cb) => {
+
+    try {
+       const { records } = await airtable.list();
+       const products = records.map((product)=> {
+        const {id} = product;
+        const {name,image,price} = product.fields
+        const url = image[0].url
+        return{id,name,url,price}
+       })
+       return {
+           statusCode:200,
+           body: JSON.stringify(products),
+       }
+    } catch (error) {
+        return {
+            statusCode:404,
+            body: 'server error',
+        }
+    }
+    
+}
